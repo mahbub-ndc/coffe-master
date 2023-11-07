c5d2ac5056d210229cc75c3c6171c09357b4aa2e
Mahbub <mahbub5015@gmail.com>
2023-11-07 15:00:11 +0600

    server code added

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 //middleware
 
-app.use(express());
+app.use(cors());
 app.use (express.json());
 
-console.log(process.env.S3_BUCKET);
+
 
 const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.71kzypr.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri)
-
-//const uri = "mongodb+srv://coffe-master:urZY3xLBotcvaYch@cluster0.71kzypr.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +27,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+    app.get('/coffee', async(req, res) =>{
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.delete('/coffee/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    app.post('/coffee', async(req, res)=>{
+        const coffeeDetails = req.body;
+        console.log(coffeeDetails);
+        const result = await coffeeCollection.insertOne(coffeeDetails);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
